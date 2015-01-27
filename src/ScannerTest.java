92a100861a28cbb80ea823585ce7686dad82c232
Doonut <kylecasey4@hotmail.com>
2015-01-27 08:51:57 -0800

    ScannerTest

---
+++ src/ScannerTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+	public static void main(String[] args)
+	{
+		int num1 = 0;
+		double num2 = 0;
+		String str,str1 = " ";
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		num1 = input.nextInt();
+		System.out.print("Enter a decimal number: ");
+		num2 = input.nextDouble();
+		System.out.print("Enter your name: ");
+		str = input.nextLine();
+		str1 = input.next(); 
+		
+		System.out.println("The number is " + num1 + " and the floating point is " + num2 + " and your name is " + str1 +".");
+	}
+}
