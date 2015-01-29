98146bcaeeb3c43dd56bc4a9947f19461fdf118b
Donut <kylecasey4@hotmail.com>
2015-01-28 18:14:37 -0800

    fixed warnings on ScannerTest

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -5,7 +5,8 @@ public class ScannerTest {
 	{
 		int num1 = 0;
 		double num2 = 0;
-		String str,str1 = " ";
+		@SuppressWarnings("unused")
+		String str = " ",str1 = " ";
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -14,9 +15,9 @@ public class ScannerTest {
 		System.out.print("Enter a decimal number: ");
 		num2 = input.nextDouble();
 		System.out.print("Enter your name: ");
-		str = input.nextLine();
 		str1 = input.next(); 
-		
-		System.out.println("The number is " + num1 + " and the floating point is " + num2 + " and your name is " + str1 +".");
+		str = input.nextLine();
+		input.close();
+		System.out.println("The number is " + num1 + " and the floating point is " + num2 + " and your name is " + str1 + " " + str + ".");
 	}
 }
