f60e0185fd7baf356142b1edb96630e34bac5f27
Donut <kylecasey4@hotmail.com>
2015-02-06 13:06:43 -0800

    readwrite

---
+++ src/Project2ReadWrite.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Project2ReadWrite {
+	public static String inFileName = "in.txt", outFileName = "out.txt";
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Scanner inFile = new Scanner(new FileReader(inFileName));
+		PrintWriter outFile = new PrintWriter(outFileName);
+		
+	}
+}
