62b9d4b4deed3010a1c5cf7b573df689d64639c3
Donut <kylecasey4@hotmail.com>
2015-03-09 22:22:44 -0700

    BETA: 0.5

--- src/myJava/borasmom/MainWindow.java
+++ src/myJava/borasmom/MainWindow.java
@@ -6,18 +6,16 @@ import java.util.Random;
 import javax.swing.JFrame;
 
 public class MainWindow implements Runnable {
-	public static JFrame frame = new JFrame("Bora's Mom BETA: 0.4");
+	public static JFrame frame = new JFrame("Bora's Mom BETA: 0.5");
 	
 	public void run() {
 		
 		Random rand = new Random();
 		
-		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.setResizable(false);
 		frame.setSize(800, 800);
-		
-		
 		
 		while(true)
 		{
@@ -27,6 +25,7 @@ public class MainWindow implements Runnable {
 			Color color = new Color(r,g,b);
 			frame.getContentPane().setBackground(color);
 			color = null;
+			
 			try {
 			    Thread.sleep(100);                 //1000 milliseconds is one second.
 			} catch(InterruptedException ex) {
@@ -34,7 +33,6 @@ public class MainWindow implements Runnable {
 			}
 			
 		}
-		
 	}
 }
 
--- src/myJava/borasmom/Text.java
+++ src/myJava/borasmom/Text.java
@@ -1,6 +1,7 @@
 package myJava.borasmom;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.util.Random;
 
 import javax.swing.JTextArea;
@@ -8,20 +9,30 @@ import javax.swing.JTextArea;
 public class Text implements Runnable {
 	public void run(){
 		Color color2 = new Color(0,0,0,0);
-		JTextArea mainText = new JTextArea(5,20);
+		
+		JTextArea mainText = new JTextArea();
 		mainText.setEditable(false);
 		mainText.append("BUCKED FORAS MOM");
 		mainText.setBackground(color2);
-		mainText.setBounds(330,355,230,90);
+		Font f = mainText.getFont();
+		Font f2 = new Font(f.getFontName(), f.getStyle(), f.getSize()+10);
+		mainText.setFont(f2);
+		
 		Random rand = new Random();
+		
 	while(true){
 		float r = rand.nextFloat();
 		float g = rand.nextFloat();
 		float b = rand.nextFloat();
+		int x = rand.nextInt((500 - 10) + 1) + 10;
+		int y = rand.nextInt((700 - 100) + 1) + 100;
+		mainText.setBounds(x,y,240,90);
 		Color color = new Color(r,g,b);
 		mainText.setForeground(color);
 		color = null;
+		
 		MainWindow.frame.add(mainText);
+		
 		try {
 		    Thread.sleep(100);                 //1000 milliseconds is one second.
 		} catch(InterruptedException ex) {
